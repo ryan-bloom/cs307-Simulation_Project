ceb5eb221d764760ff77aa37d19344bff374f5c2
Matt Rose <mjr68@duke.edu>
2019-02-18 17:18:37 -0500

    changed arrayList of states to a 2D array in Data class

--- src/Data.java
+++ src/Data.java
@@ -1,14 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Data {
 
     private String simulation;
     private int height, width;
-    private List<Integer> states;
+    private int[][] states;
 
     public Data(File file) throws FileNotFoundException {
         Scanner scanner = new Scanner(file);
@@ -16,8 +14,12 @@ public class Data {
         simulation = scanner.next();
         height = Integer.parseInt(scanner.next().trim()); //nextInt() throws an error for me because of \r\n Windows set up
         width = Integer.parseInt(scanner.next().trim()); //trim() gets rid of \r character
-        states = new ArrayList<>();
-        while(scanner.hasNext()) states.add(Integer.parseInt(scanner.next().trim()));
+        states = new int[height][width];
+        for(int i = 0; i<height; i++){
+            for(int j = 0; j<width; j++){
+                if(scanner.hasNext()) states[i][j] = Integer.parseInt(scanner.next().trim());
+            }
+        }
     }
 
     public String getSimulation(){
@@ -29,7 +31,7 @@ public class Data {
     public int getWidth() {
         return width;
     }
-    public List<Integer> getStates(){
+    public int[][] getStates(){
         return states;
     }
 
