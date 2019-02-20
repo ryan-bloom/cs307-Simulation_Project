15d0e29cad3ea86cdf19432ea7c82ab22ea6e85b
Matt Rose <mjr68@duke.edu>
2019-02-19 19:31:27 -0500

    updated Data class, tested in Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+
 public class Main{
-    public static void main (String[] args) {
-        System.out.println("HELLO");
+    public static void main (String[] args) throws FileNotFoundException, URISyntaxException {
+        Data test  = new Data("GameOfLife_Config_1.txt");
+        System.out.println(test.getSimulation());
+        System.out.println(test.getHeight() + "  " + test.getWidth());
+        for(int i = 0; i<test.getHeight();i++){
+            for(int j = 0; j<test.getWidth(); j++){
+                System.out.print(test.getStates()[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
