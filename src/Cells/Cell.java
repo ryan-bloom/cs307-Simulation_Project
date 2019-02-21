a500a51cf023d45e1ef04ca0d920cf4d6224a947
Matt Rose <mjr68@duke.edu>
2019-02-21 18:33:11 -0500

    fixed index error in Data class

--- src/Cells/Cell.java
+++ src/Cells/Cell.java
@@ -14,8 +14,9 @@ abstract public class Cell {
         myRow = row;
         myCol = col;
         myCurrentState = state;
-        myNextState = -1;
+        myNextState = state;
         myRectangle = new Rectangle(row * width, col * height, width, height);
+        this.updateColor();
     }
 
     public Rectangle getShape() {
