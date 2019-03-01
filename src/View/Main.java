ac500a03858930306387f1610a125a7dabc6a98d
Rohan Reddy <rr195@duke.edu>
2019-03-01 03:05:27 -0500

    JavaFX is the worst. Added custom color functionality, custom image functionality, with a toggle button to choose between the two. UI design is admittedly atrocious at the moment but will be easy to fix in Complete (hopefully) since it is not design dependent

--- src/View/Main.java
+++ src/View/Main.java
@@ -7,22 +7,38 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import javafx.geometry.Insets;
+import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
+import javafx.event.*;
+import javafx.scene.image.ImageView;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.ResourceBundle;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import javafx.scene.text.*;
 
 public class Main extends Application {
 
     public static final String SIMULATION = "Percolation";
+    public static final int ACTUAL_WINDOW_WIDTH = 1000;
     public static final int WINDOW_HEIGHT = 700;
     public static final int WINDOW_WIDTH = 700;
+    //public static final String STYLESHEET = "styles.css";
 
     public static int FRAMES_PER_SECOND = 6;
     public static double SECOND_DELAY = 3.0 / FRAMES_PER_SECOND;
@@ -37,8 +53,8 @@ public class Main extends Application {
     private ResourceBundle myResources;
     private Stage myStage;
 
-
-    private HashMap<Integer, Color> colors = new HashMap<>();
+    private HashMap<Integer, Color> cellColors = new HashMap<>();
+    private HashMap<Integer, Image> cellImages = new HashMap<>();
     private double cellWidth;
     private double cellHeight;
     private boolean isRunning = true;
@@ -49,14 +65,39 @@ public class Main extends Application {
 
     public void start(Stage stage) {
         myStage = stage;
+        myGroup = new Group();
         myStage.setScene(setupSeed(1));
         myStage.setTitle(mySeed.getSimulation());
+        RadioButton cellChoiceImage = new RadioButton("Image");
+        cellChoiceImage.relocate(700, 500);
+        myGroup.getChildren().add(cellChoiceImage);
+        Button toRun = new Button("Run simulation");
+        toRun.setOnAction((ActionEvent event) -> {
+            if (cellChoiceImage.isSelected()) {
+                final FileChooser fileChooser = new FileChooser();
+                fileChooser.getExtensionFilters().addAll(
+                        new FileChooser.ExtensionFilter("All Images", "*.*"),
+                        new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+                        new FileChooser.ExtensionFilter("PNG", "*.png")
+                );
+                List<File> imageFiles = fileChooser.showOpenMultipleDialog(stage);
+                if (imageFiles != null) {
+                    for (int i = 0; i < imageFiles.size(); i++) {
+                        Image image = new Image(imageFiles.get(i).toURI().toString());
+                        cellImages.put(i, image);
+                    }
+                }
+            }
+            var frame = new KeyFrame(Duration.seconds(SECOND_DELAY), e -> step());
+            myAnimation = new Timeline();
+            myAnimation.setCycleCount(Timeline.INDEFINITE);
+            myAnimation.getKeyFrames().add(frame);
+            myAnimation.play();
+        });
+        toRun.relocate(700, 200);
+        myGroup.getChildren().add(toRun);
         myStage.show();
-        var frame = new KeyFrame(Duration.seconds(SECOND_DELAY), e -> step());
-        myAnimation = new Timeline();
-        myAnimation.setCycleCount(Timeline.INDEFINITE);
-        myAnimation.getKeyFrames().add(frame);
-        myAnimation.play();
+
     }
 
     public Scene setupSeed(int config) {
@@ -67,8 +108,17 @@ public class Main extends Application {
         cellHeight = WINDOW_HEIGHT/mySeed.getHeight();
         cellWidth = WINDOW_WIDTH/mySeed.getWidth();
         myGrid.fillCellGrid();
-        myGroup = new Group();
-        Scene seed = new Scene(myGroup, WINDOW_WIDTH, WINDOW_HEIGHT, BACKGROUND);
+        Scene seed = new Scene(myGroup, ACTUAL_WINDOW_WIDTH, WINDOW_HEIGHT, BACKGROUND);
+
+        for (Integer i : cellColors.keySet()) {
+            final ColorPicker colorPicker = new ColorPicker();
+            colorPicker.setOnAction((ActionEvent t) -> {
+                cellColors.put(i, colorPicker.getValue());
+            });
+            colorPicker.relocate(700, 40 * i);
+            myGroup.getChildren().add(colorPicker);
+        }
+
         for (int i = 0; i < mySeed.getWidth(); i++) {
             for (int j = 0; j < mySeed.getHeight(); j++) {
                 Node view = updateCellView(i, j, myGrid.getCellState(i, j));//cellGrid[i][j].getMyCurrentState());
@@ -76,6 +126,7 @@ public class Main extends Application {
             }
         }
         seed.setOnKeyPressed(e -> handleKeyInput(e.getCode()));
+        //seed.getStylesheets().add(STYLESHEET);
         return seed;
     }
 
@@ -85,17 +136,26 @@ public class Main extends Application {
             String value = myResources.getString(s);
             if(s.contains("Color")) {
                 String[] color = value.split(",");
-                colors.put(Integer.parseInt(color[0]), Color.valueOf(color[1]));
+                cellColors.put(Integer.parseInt(color[0]), Color.valueOf(color[1]));
             }
         }
     }
 
-    //GOING TO HAVE TO HANDLE RECTANGLES AND IMAGE VIEWS EVENTUALLY
-    public Node updateCellView(int row, int col, int state){
-        var res = new Rectangle(row * cellWidth, col * cellHeight, cellWidth, cellHeight);
-        var color = colors.get(state);
-        res.setFill(color);
-        return res;
+    public Node updateCellView(int row, int col, int state) {
+        if (cellImages.get(state) != null) {
+            var res = new ImageView(cellImages.get(state));
+            res.setFitHeight(cellHeight);
+            res.setFitWidth(cellWidth);
+            res.setX(row * cellWidth);
+            res.setY(col * cellHeight);
+            return res;
+        }
+        else {
+            var res = new Rectangle(row * cellWidth, col * cellHeight, cellWidth, cellHeight);
+            var color = cellColors.get(state);
+            res.setFill(color);
+            return res;
+        }
     }
 
     private void step() {
