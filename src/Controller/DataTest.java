306d7c30be3ea34f4c09590d6a813892534458da
Matt Rose <mjr68@duke.edu>
2019-03-08 23:09:55 -0500

    updated readme, DataTest

--- src/Controller/DataTest.java
+++ src/Controller/DataTest.java
@@ -8,13 +8,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DataTest {
     private final String TEST_FILE = "DataTest_Config_1.csv";
-    private final String EMPTY_FILE = "DataTest_Config_2.csv";
-    private Data d,e;
+    private Data d, f;
 
     @BeforeEach
     void setUp(){
         d = new Data(TEST_FILE);
-        e = new Data(EMPTY_FILE);
+        f = new Data(new int[]{10,5,10}, 5,5);
     }
 
     @Test
@@ -23,18 +22,8 @@ class DataTest {
     }
 
     @Test
-    void getHeightWithEmptyFile(){
-        assertEquals(0, e.getHeight());
-    }
-
-    @Test
     void getWidth() {
         assertEquals(5, d.getWidth());
-    }
-
-    @Test
-    void getWidthWithEmptyFile(){
-        assertEquals(0, e.getWidth());
     }
 
     @Test
@@ -45,10 +34,21 @@ class DataTest {
         }
         assertArrayEquals(arr, d.getStates());
     }
-
     @Test
-    void getStatesWithEmptyFile(){
-        int[][] arr = new int[0][0];
-        assertArrayEquals(arr, e.getStates());
+    void getRandomStates(){
+        int[][] arr = f.getStates();
+        int sum1 = 0;
+        int sum2 = 0;
+        int sum3 = 0;
+        for(int i=0; i<f.getHeight(); i++){
+            for(int j =0; j<f.getWidth(); j++){
+                if(f.getStates()[i][j]==0) sum1++;
+                else if(f.getStates()[i][j]==1) sum2++;
+                else if(f.getStates()[i][j]==2) sum3++;
+            }
+        }
+        assertEquals(10, sum1);
+        assertEquals(5, sum2);
+        assertEquals(10, sum3);
     }
 }
